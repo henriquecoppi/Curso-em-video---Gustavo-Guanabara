2907acb4f4b389219ccce05048f3783a039dbcfb
henriquecoppi <65692694+henriquecoppi@users.noreply.github.com>
2022-01-04 22:59:16 -0300

    Primerio commit curso em video

---
+++ Curso em video - Gustavo Guanabara/Curso em Vídeo - JavaScript/11_condição - parte1/main02.js
@@ -0,0 +1,10 @@
+var velocidade = 100
+console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+if(velocidade > 70){
+    console.log('Voce utrapasou a velocidade permitida. Multado')
+    
+} 
+
+//console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+//console.log('Dirija sempre usando cinto de segurança')
+//console.log('Voce utrapasou a velocidade permitida. Multado')
---
+++ Curso em video - Gustavo Guanabara/Curso em Vídeo - JavaScript/11_condição - parte1/main03.js
@@ -0,0 +1,15 @@
+var velocidade = 80
+
+if(velocidade <= 70){
+    console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+}if (velocidade >70) {
+    console.log('Voce utrapasou a velocidade permitida. Multado')
+
+    
+} else {
+    console.log('Dirija sempre usando cinto de segurança')
+}
+
+//console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+//console.log('Dirija sempre usando cinto de segurança')
+//console.log('Voce utrapasou a velocidade permitida. Multado')
